088e8f0e1770e1edf20effa9b236b9ebd6cc434c
nischalm <nischalmohan95@gmail.com>
2021-02-16 16:06:30 +0530

    lesson 6 final

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Navbar from './navbar'
 import Home from './home'
 
@@ -12,14 +11,6 @@ function App() {
       <Navbar />
       <div className="content">
         <Home />
-
-        <p> { 10 } </p>
-        <p> {'hello ninjas'} </p>
-        <p> {[1, 2, 3, 4, 5]} </p>
-        <p> {Math.random() * 10} </p>
-
-
-        <a href={link}>Google Site</a>
       </div>
     </div>
   );
