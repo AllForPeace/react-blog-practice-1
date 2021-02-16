d6621d14f31551b39b1cab4d83ea1331ef223224
nischalm <nischalmohan95@gmail.com>
2021-02-16 15:47:59 +0530

    lesson 5 final

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import './App.css';
+import Navbar from './navbar'
+import Home from './home'
 
 function App() {
   const title = 'Welcome to the new blog';
   const likes = 50;
-  // const person = { name: 'yoshi', age: 30}
   const link = "https://www.google.com";
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-
-        {/* <p> { person } </p> */}
+        <Home />
 
         <p> { 10 } </p>
         <p> {'hello ninjas'} </p>
