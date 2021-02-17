5c05af98b79d5675e1f28e122f3bde1bce1b4cd1
nischalm <nischalmohan95@gmail.com>
2021-02-17 12:59:43 +0530

    lesson 19

--- src/home.js
+++ src/home.js
@@ -4,23 +4,32 @@ import BlogList from './BlogList'
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
-
-    const [name, setName] = useState('mario');
+    const [error, setError] = useState(null);
 
     useEffect(() => setTimeout(() => {
         fetch('http://localhost:8000/blogs')
-        .then(res => res.json())
-        .then(data => {
-            setBlogs(data);
-            setIsPending(false);
-        })
+            .then(res => {
+                if(!res.ok) {
+                    throw Error('could not fetch data');
+                }
+                console.log(res);
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                setIsPending(false);
+                setError(err.message);
+            })
     }, 1000), []); /* setTimeout only for display of loading screen visible to the human eye */
     return (
         <div className="home">
-            { isPending && <div>Loading ... </div>}
+            {isPending && <div>Loading ... </div>}
+           { error && <div>{ error }</div> }
            {blogs && <BlogList blogs= {blogs} title="All Blogs!"/>}
-           <button onClick={ () => setName ('luigi') }>Change Name</button>
-           <p> { name } </p>
         </div>
     );
 }
