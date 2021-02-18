5efb21f36c95e08e5d0ff4ea34c0dcd306f23161
nischalm <nischalmohan95@gmail.com>
2021-02-18 12:57:59 +0530

    lesson 20

--- src/home.js
+++ src/home.js
@@ -1,30 +1,10 @@
-import { useState, useEffect } from 'react';
 import BlogList from './BlogList'
+import useFetch from './useFetch'
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => setTimeout(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                if(!res.ok) {
-                    throw Error('could not fetch data');
-                }
-                console.log(res);
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-                setIsPending(false);
-                setError(null);
-            })
-            .catch(err => {
-                setIsPending(false);
-                setError(err.message);
-            })
-    }, 1000), []); /* setTimeout only for display of loading screen visible to the human eye */
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
+    //use colon to instantiate local const in array/object destruction
+    
     return (
         <div className="home">
             {isPending && <div>Loading ... </div>}
