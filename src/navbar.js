e075a98864894e229a74068f2042fbec12824b2c
nischalm <nischalmohan95@gmail.com>
2021-03-06 18:42:08 +0530

    nav logo now links to home page

--- src/navbar.js
+++ src/navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 function Navbar() {
     return (
         <nav className="navbar">
-            <h1>The Dojo Blog</h1>
+            <Link className="nav-logo-link" to="/"><h1>The Dojo Blog</h1></Link>
             <div className="links">
                 <Link to="/">Home</Link>
                 <Link to="/create">New Blog</Link>
